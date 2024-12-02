5f21e5cb898115f79ffede092571a0c9b2ce04d2
chandrakamalsingh007 <chandrakamalsingh007@gmail.com>
2024-12-02 18:24:19 +0545

    variables concept

--- 01variables/Variables.py
+++ 01variables/Variables.py
@@ -1,4 +1,4 @@
-# Variables - A variable is a name given to a memory location in a program.
+# Variables - A variable is a name given to a memory location in a program that holds a value.
 
 name = "Chandra"
 age = 23
@@ -9,3 +9,9 @@ print(type(name),type(age),type(price))
 # print(price)
 
 print("my name is ",name," My age is ",age,"Price is ",price)
+
+
+# Data-types:- The type of value that is assign to variable.
+# integer - without decimal points
+# float - with decimal points
+# string - any word or a collection of characters
